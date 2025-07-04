d2a938ff9f8e6739e8ab0e1bf36feb548a6afb2e
fantic14 <filip.antic14@gmail.com>
2025-07-04 14:37:31 +0200

    init

---
+++ src/main/java/org/fantic/ethcrawler/EthereumCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.fantic.ethcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthereumCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fantic/ethcrawler/EthereumCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fantic.ethcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthereumCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
